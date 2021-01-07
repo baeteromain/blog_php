67ef633e5032a9bb1c0efb34db32de6fe286a100
BAETE <baete.romain.code@gmail.com>
2021-01-07 15:36:57 +0100

    :recycle: Refactor test code to send mail in index with new mailer class #5

--- public/index.php
+++ public/index.php
@@ -1,48 +1,18 @@
 <?php
 
+use App\core\Mailer;
 use App\core\Router;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-use PHPMailer\PHPMailer\PHPMailer;
 
 require '../vendor/autoload.php';
 require '../config/database.php';
 require '../config/mailer_config.php';
 
-
-
 $router = new Router();
 
 $router->run();
 
-$mail = new PHPMailer(true);
+$mail = new Mailer(true, EMAIL_USERNAME);
 
-try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-    $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = EMAIL_HOST;                    // Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = EMAIL_USERNAME;                     // SMTP username
-    $mail->Password   = EMAIL_PASSWORD;                               // SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = EMAIL_PORT;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
-
-    //Recipients
-    $mail->setFrom(EMAIL_USERNAME, 'Romain');
-    $mail->addAddress('baete.romain@gmail.com');     // Add a recipient
-    
-
-
-
-    // Content
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-}
+$mail->Subject ='sujet test';
+$mail->Body = "<h1>TEST</h1>";
+$mail->send();
