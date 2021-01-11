ff8591977b150d6faa363d708211d43a02392aef
BAETE <baete.romain.code@gmail.com>
2021-01-11 11:21:47 +0100

    :construction: Add new method Signin to ckeck field after submit form #6

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,11 +2,25 @@
 namespace App\Controller;
 
 use App\core\Controller;
+use App\core\Validation\Validator;
 
 class RegisterController extends Controller{
 
     public function Register()
     {
+        return $this->render('register/index.twig');
+    }
+
+    public function Signin()
+    {
+        if(isset($_POST)){
+           $validations = new Validator($_POST);
+           $errors = $validations->validate('register');
+           return $this->render('register/index.twig', [
+            'errors' => $errors,
+            'post' => $_POST
+            ]);
+        }
         return $this->render('register/index.twig');
     }
 }
