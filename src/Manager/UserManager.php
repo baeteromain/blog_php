a18866c2f0315496e9ed646c0d59fa32d498baa0
BAETE <baete.romain.code@gmail.com>
2021-01-14 07:51:57 +0100

    🚧 Add new UserManager class with createUser method #6

---
+++ src/Manager/UserManager.php
@@ -0,0 +1,22 @@
+<?php 
+namespace App\Manager;
+
+use App\core\Database;
+
+class UserManager extends Database{
+
+    public function createUser($username, $email, $password, $role){
+
+        return $this->createQuery('
+            INSERT INTO user (username, email, password, role_id)
+            VALUES (:username, :email, :password, :role)',
+                [
+                    'username' => $username,
+                    'email' => $email,
+                    'password' => $password,
+                    'role' => $role,
+                ]
+        );
+    }
+
+}
