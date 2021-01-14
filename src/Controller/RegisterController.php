430291eb046ed750134e85475e3fed223d03ffc9
BAETE <baete.romain.code@gmail.com>
2021-01-14 07:56:24 +0100

    🚧 Render view with error or create new user #6

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -3,24 +3,31 @@ namespace App\Controller;
 
 use App\core\Controller;
 use App\core\Validation\Validator;
+use App\Manager\UserManager;
 
 class RegisterController extends Controller{
 
-    public function Register()
-    {
-        return $this->render('register/index.twig');
-    }
+    const SUBSCRIBER = 1;
+    const ADMIN = 2;
 
-    public function Signin()
+    public function signin()
     {
-        if(isset($_POST)){
+        if(!empty($_POST)){
+        
            $validations = new Validator($_POST);
            $errors = $validations->validate('register');
-           return $this->render('register/index.twig', [
-            'errors' => $errors,
-            'post' => $_POST
-            ]);
+           if(empty($errors)){
+            
+               $user = new UserManager;
+               $user->createUser($_POST['username'], $_POST['email'], $_POST['password'], self::SUBSCRIBER);
+               header('Location: /');
+               exit;
+           }
+           
         }
-        return $this->render('register/index.twig');
+        return $this->render('register/index.twig', [
+            'errors' => $errors ?? null,
+            'post' => $_POST ?? null
+            ]);
     }
 }
