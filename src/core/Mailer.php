7636abd74a141ca083d5b669034dd87336fe01c2
BAETE <baete.romain.code@gmail.com>
2021-01-15 10:24:16 +0100

    🚧 Add template body for mailer #6

--- src/core/Mailer.php
+++ src/core/Mailer.php
@@ -7,7 +7,7 @@ use PHPMailer\PHPMailer\PHPMailer;
 
 class Mailer extends PHPMailer{
 
-    public function __construct($exceptions, string $to)
+    public function __construct($exceptions, string $to, string $username, string $template)
     {
         parent::__construct($exceptions);
         #$this->SMTPDebug = SMTP::DEBUG_SERVER;
@@ -20,7 +20,14 @@ class Mailer extends PHPMailer{
         $this->Port       = EMAIL_PORT;
         $this->isHTML(true);
         $this->setFrom(EMAIL_USERNAME, 'Romain');
-        $this->addAddress($to);  
+        $this->addAddress($to);
+        if ($template === 'register'){
+            $this->Subject = 'Inscription au blog de Romain';
+            $message = file_get_contents('../templates/register/mail.html');
+            $message =str_replace('%username%', $username, $message);
+            $message =str_replace('%email%', $to, $message);
+            $this->Body = $message;
+        } 
         
     }
 
