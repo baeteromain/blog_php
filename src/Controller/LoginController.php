87c5f30772eb46af736eca432b722a1473059d7f
BAETE <baete.romain.code@gmail.com>
2021-01-18 11:49:08 +0100

    🚧 Implement checking and rendering error in LoginController and add redirection #7

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,12 +2,34 @@
 namespace App\Controller;
 
 use App\core\Controller;
+use App\Manager\UserManager;
+use App\core\Validation\Validator;
 
 class LoginController extends Controller{
 
     public function login()
     {
-        return $this->render('login/index.twig');
+        $post = $this->post;
+        $userManager = new UserManager;
+         if(!empty($post)){
+            $validations = new Validator($post);
+            $errors = $validations->validate('login');
+            if(empty($errors)){
+                $result = $userManager->login($post['username'], $post['password']);
+                if ($result !== true){
+                $errors['login'] = $result;
+                }
+            }
+
+            if(empty($errors)){
+                header('Location: /');
+                exit;          
+            }
+         }
+        return $this->render('login/index.twig', [
+            'errors' => $errors ?? null,
+            'post' => $post ?? null
+            ]);
     }
 
 }
