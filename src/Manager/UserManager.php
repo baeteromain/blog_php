bccf5a595fbf7edd6a0cfa17b770595f4f75239f
BAETE <baete.romain.code@gmail.com>
2021-02-05 10:08:58 +0100

    ♻️ Modify updateUser to updateUsername and updateEmail method #9

--- src/Manager/UserManager.php
+++ src/Manager/UserManager.php
@@ -24,16 +24,29 @@ class UserManager extends Database
         );
     }
 
-    public function updateUser($id, $username, $email)
+    public function updateUsername($id, $username)
     {
         return $this->createQuery(
             '
-        UPDATE user SET username = :username , email = :email
+        UPDATE user SET username = :username 
         WHERE id = :id',
             [
                 'id' => $id,
                 'username' => $username,
+            ]
+        );
+    }
+
+    public function updateEmail($id, $email, $token)
+    {
+        return $this->createQuery(
+            '
+        UPDATE user SET email = :email, token = :token, valid = 0
+        WHERE id = :id',
+            [
+                'id' => $id,
                 'email' => $email,
+                'token' => $token,
             ]
         );
     }
