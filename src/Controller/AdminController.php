6d6438346c77918c6cb12099a3edb7b3cd10de9f
BAETE <baete.romain.code@gmail.com>
2021-02-15 11:36:28 +0100

    ✨ Add method to reset email or password in adminController

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,10 +3,17 @@
 namespace App\Controller;
 
 use App\core\Controller;
+use App\core\Mailer;
+use App\core\Validation\Validation;
 use App\Manager\UserManager;
 
 class AdminController extends Controller
 {
+    const ROLE = [
+        'subscriber' => 1,
+        'admin' => 2,
+    ];
+
     private $userManager;
 
     public function __construct()
@@ -14,6 +21,7 @@ class AdminController extends Controller
         parent::__construct();
 
         $this->userManager = new UserManager();
+        $this->validator = new Validation();
     }
 
     public function index()
@@ -34,26 +42,27 @@ class AdminController extends Controller
         ]);
     }
 
-    public function upgradeUser()
+    public function updateUserRole()
     {
         $this->checkAdmin();
 
         $user = $this->userManager->getUserById($this->get['id']);
 
-        if (!empty($this->get) && isset($this->get['id']) && $user) {
-            if ('1' === $user->getRole_id()) {
-                $this->userManager->upgradeRole($user->getId(), '2');
+        if (!empty($this->get) && isset($this->get['id'], $this->get['role']) && $user) {
+            if ('subscriber' === $this->get['role'] || 'admin' === $this->get['role']) {
+                if ('admin' === $this->get['role']) {
+                    $this->userManager->updateRole($user->getId(), self::ROLE['admin']);
+                }
+                if ('subscriber' === $this->get['role']) {
+                    $this->userManager->updateRole($user->getId(), self::ROLE['subscriber']);
+                }
+
+                $this->session->set('upgrade_user', 'Le role de l\'utilisateur à bien été changé');
+
+                header('Location: /admin/users');
+
+                exit();
             }
-
-            if ('2' === $user->getRole_id()) {
-                $this->userManager->upgradeRole($user->getId(), '1');
-            }
-
-            $this->session->set('upgrade_user', 'Le role de l\'utilisateur à bien été changé');
-
-            header('Location: /admin/users');
-
-            exit();
         }
 
         $this->session->set('error_upgrade_user', 'Un problème est survenu lors du changement du role de l\'utilisateur');
@@ -63,6 +72,69 @@ class AdminController extends Controller
         return $this->render('admin/admin_users/index.twig', [
             'users' => $users,
         ]);
+    }
+
+    public function reset()
+    {
+        $this->checkAdmin();
+
+        $user = $this->userManager->getUserById($this->get['id']);
+
+        if (!empty($this->get) & isset($this->get['email'])) {
+            $errors = $this->validator->validate($this->get, 'User');
+
+            if (!$errors) {
+                $this->resetEmail($user->getId(), $user->getUsername());
+            }
+        }
+
+        return $this->render('admin/admin_users/reset.twig', [
+            'user' => $user,
+            'errors' => $errors ?? null,
+        ]);
+    }
+
+    public function resetEmail($id, $username)
+    {
+        $token = bin2hex(openssl_random_pseudo_bytes(16));
+
+        $this->userManager->updateUser($id, $username, $this->get['email'], $token, '0');
+
+        $mailer = new Mailer(true, $this->get['email'], 'update');
+
+        $mailer->Body = $this->renderMail('profil/mail_confirm_email.html.twig', [
+            'email' => $this->get['email'],
+            'url' => Mailer::url(),
+            'token' => $token,
+        ]);
+        $mailer->send();
+
+        header('Location: /admin/users');
+
+        exit();
+    }
+
+    public function resetPassword()
+    {
+        $this->checkAdmin();
+
+        $password = bin2hex(openssl_random_pseudo_bytes(8));
+        $user = $this->userManager->getUserById($this->get['id']);
+
+        $this->userManager->updatePassword($password, $user->getEmail());
+
+        $mailer = new Mailer(true, $user->getEmail(), 'resetPassword');
+
+        $mailer->Body = $this->renderMail('admin/admin_users/mail_reset_password.html.twig', [
+            'email' => $user->getEmail(),
+            'url' => Mailer::url(),
+            'password' => $password,
+        ]);
+        $mailer->send();
+
+        header('Location: /admin/users');
+
+        exit();
     }
 
     public function deleteUsers()
