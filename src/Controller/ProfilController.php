998af0d967baf0c042c171e722963e2bad4647e8
BAETE <baete.romain.code@gmail.com>
2021-02-15 11:33:00 +0100

    ♻️ use method updateUser to update email or username

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -29,7 +29,7 @@ class ProfilController extends Controller
     {
         $this->checkLoggedIn();
         $user = $this->session->get('user');
-
+        // RECUPERE EN USER , UN SEUL IF
         if (!empty($this->post) && isset($this->post['username'], $this->post['email'])) {
             if ($this->post['username'] != $user['username'] || $this->post['email'] != $user['email']) {
                 $errors = $this->validator->validate($this->post, 'User');
@@ -41,29 +41,29 @@ class ProfilController extends Controller
                     unset($errors['username']);
                 }
 
-                if (!$errors && $user['email'] != $this->post['email']) {
-                    $token = bin2hex(openssl_random_pseudo_bytes(16));
-                    $this->userManager->updateEmail($user['id'], $this->post['email'], $token);
-                    $this->session->remove('user');
-
-                    $mailer = new Mailer(true, $this->post['email'], 'update');
-
-                    $mailer->Body = $this->renderMail('profil/mail_confirm_email.html.twig', [
-                        'email' => $this->post['email'],
-                        'url' => Mailer::url(),
-                        'token' => $token,
-                    ]);
-                    $mailer->send();
-
-                    $this->session->set('update_email', 'Votre lien d\'activation vous a été envoyé à votre nouvelle adresse');
-
-                    header('Location: /login');
-
-                    exit();
-                }
-
                 if (!$errors) {
-                    $this->userManager->updateUsername($user['id'], $this->post['username']);
+                    if ($user['email'] != $this->post['email']) {
+                        $token = bin2hex(openssl_random_pseudo_bytes(16));
+                        $this->userManager->updateUser($user['id'], $user['username'], $this->post['email'], $token, '0');
+                        $this->session->remove('user');
+
+                        $mailer = new Mailer(true, $this->post['email'], 'update');
+
+                        $mailer->Body = $this->renderMail('profil/mail_confirm_email.html.twig', [
+                            'email' => $this->post['email'],
+                            'url' => Mailer::url(),
+                            'token' => $token,
+                        ]);
+                        $mailer->send();
+
+                        $this->session->set('update_email', 'Votre lien d\'activation vous a été envoyé à votre nouvelle adresse');
+
+                        header('Location: /login');
+
+                        exit();
+                    }
+
+                    $this->userManager->updateUser($user['id'], $this->post['username'], $user['email']);
                     $this->session->update('user', 'username', $this->post['username']);
 
                     header('Location: /profil');
