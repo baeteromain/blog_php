3d2e9d4452b9b8247276b03373c7ad5883f7f4e9
BAETE <baete.romain.code@gmail.com>
2021-03-05 14:38:30 +0100

    ✨ Add new route to delete post #18

--- src/core/Router.php
+++ src/core/Router.php
@@ -41,6 +41,7 @@ class Router extends AltoRouter
         $this->map('POST', '/admin/posts/add', 'PostController#addPost');
         $this->map('GET', '/admin/posts/update', 'PostController#updatePost');
         $this->map('POST', '/admin/posts/update', 'PostController#updatePost');
+        $this->map('GET', '/admin/posts/delete', 'PostController#deletePost');
     }
 
     public function run()
