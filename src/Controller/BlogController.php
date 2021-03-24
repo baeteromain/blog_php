93f3114f0c153620af5b6e74945810bf27fc03e5
BAETE <baete.romain.code@gmail.com>
2021-03-24 11:27:13 +0100

    ✨ Add new method addComment #22

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\core\Controller;
+use App\core\Validation\Validation;
 use App\Manager\CategoryManager;
+use App\Manager\CommentManager;
 use App\Manager\PostManager;
 use App\Model\Pagination;
 
@@ -12,6 +14,7 @@ class BlogController extends Controller
     private $postManager;
     private $categoryManager;
     private $pagination;
+    private $commentManager;
 
     public function __construct()
     {
@@ -20,6 +23,8 @@ class BlogController extends Controller
         $this->postManager = new PostManager();
         $this->categoryManager = new CategoryManager();
         $this->pagination = new Pagination();
+        $this->commentManager = new CommentManager();
+        $this->validator = new Validation();
     }
 
     public function listPosts()
@@ -47,11 +52,22 @@ class BlogController extends Controller
     public function singlePost()
     {
         if (!empty($this->get['id'])) {
+            $user = $this->session->get('user');
+
             $post = $this->postManager->getPostById($this->get['id']);
             $categoriesOfPost = $this->postManager->getCategoryByPost($this->get['id']);
         }
 
+        if (!empty($this->post)) {
+            $errors = $this->validator->validate($this->post, 'Comment');
+            if (!$errors) {
+                $this->addComment($user['id'], $this->post['content'], $this->post['post_id']);
+            }
+        }
+
         return $this->render('blog/single/index.twig', [
+            'errors' => $errors ?? null,
+            'user' => $user ?? null,
             'post' => $post ?? null,
             'categoriesOfPost' => $categoriesOfPost ?? null,
         ]);
@@ -86,5 +102,10 @@ class BlogController extends Controller
             'categories_slug' => $this->get['slug'] ?? null,
             'category_name' => $cat->getName(),
         ]);
+    }
+
+    private function addComment($user, $content, $post_id)
+    {
+        $this->commentManager->addComment($content, null, $post_id, $user);
     }
 }
